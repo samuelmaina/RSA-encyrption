1c4cb720d169a2d52af7e4f67530640abfa89fa9
samuelmayna <samuelmayna@gmail.com>
2021-06-17 17:39:54 +0300

    changed the the constructor the main methods so as to receive the number of bits of the cipher text

--- src/com/rsa/Main.java
+++ src/com/rsa/Main.java
@@ -1,14 +1,23 @@
 package com.rsa;
 
 
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Base64;
 
 public class Main {
     public static void main(String args[]) {
-        RSA rsa = new RSA();
-        String text = "I love mathematics";
+        int numberOfbits= 1024;
+        RSA rsa = new RSA(numberOfbits);
+
+        String text = "Lorem ipsum dolor sit amet consectetur" +
+                " adipisicing elit. Eligendi, necessitatibus" +
+                " quis dignissimos maxime accusantium voluptate! " +
+                "Asperiores excepturi, atque ea doloremque quo, consectetur " +
+                "dolores aliquid error alias modi maxime mollitia consequatur.";
+
         ArrayList cipherText = rsa.encryptString(text);
-        System.out.println("This is the ciphertext:" + cipherText);
         System.out.println("This is the plain text:" + rsa.decrypt(cipherText));
     }
 }
