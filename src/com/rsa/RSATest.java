c6cfe4f49b5449872096f6f85511b1e9d3a845f3
samuelmayna <samuelmayna@gmail.com>
2021-06-17 17:43:49 +0300

    Added tests to ensure text is grouped into block Added tests to ensure text is actually encrypted. Added test to ensure that the algorithm can encrypt using very long bits

--- src/com/rsa/RSATest.java
+++ src/com/rsa/RSATest.java
@@ -1,6 +1,7 @@
 package com.rsa;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.math.BigInteger;
@@ -21,14 +22,15 @@ public class RSATest {
 
     @Test
     public void shouldEncryptAndDecryptForSingleChars() {
-        RSA rsa = new RSA();
+        int numberOfBits= 1024;
+        RSA rsa = new RSA(numberOfBits);
         String message = "h";
         testRsa(rsa, message);
     }
 
     @Test
     public void shouldEncryptAndDecryptForLongString() {
-        RSA rsa = new RSA();
+        RSA rsa = new RSA(1024);
         String message = "Lorem ipsum dolor sit amet " +
                 "consectetur adipisicing elit. In sit modi" +
                 " cumque ratione aliquam laudantium qui officiis" +
@@ -37,16 +39,49 @@ public class RSATest {
         testRsa(rsa, message);
     }
 
+    @Ignore("Takes a  lot of time approx. 56 seconds")
+    @Test
+    public void shouldDoForLongBits() {
+        int largeNoOfBits= 1024*10;
+        RSA rsa = new RSA(largeNoOfBits);
+        String message = "Lorem ipsum dolor sit amet " +
+                "consectetur adipisicing elit. In sit modi";
+        testRsa(rsa, message);
+    }
+
 
     private void testRsa(RSA rsa, String message) {
         ArrayList encrypted = rsa.encryptString(message);
+        ensureTextIsDividedIntoBlocks(message, encrypted);
+        ensureTextIsEncrypted(encrypted);
+        ensureTheMessageAndTheDecryptedTextAreTheSame(rsa, message, encrypted);
+    }
+
+    private void ensureTheMessageAndTheDecryptedTextAreTheSame(RSA rsa, String message, ArrayList encrypted) {
         String decrypted = rsa.decrypt(encrypted);
         Assert.assertEquals(message, decrypted);
     }
 
+    private void ensureTextIsEncrypted(ArrayList encrypted) {
+        //The firstCipherText is bigint even if there is casting, proof of some encryption.
+        BigInteger firstCipherText= (BigInteger) encrypted.get(0);
+        int length= firstCipherText.toString().length();
+        int minimalEncryptedLength= 50;
+        //the first word of the test string is much less than minimalEncryptedLength  hence if the first produced cipher text
+        //is greater than minimalEncryptedLength, this can be used as an indication of encryption.
+        Assert.assertTrue(length> minimalEncryptedLength);
+    }
 
+    private void ensureTextIsDividedIntoBlocks(String message, ArrayList encrypted) {
 
+        //The text should be broken into words  in which each word is encypted as  a block.
+        //Words are separated by blank space.
+        int expectedNumberOfBlocks= message.split(" ").length;
 
+        int numberOfEncryptedBlocks= encrypted.size();
+
+        Assert.assertEquals(numberOfEncryptedBlocks,expectedNumberOfBlocks);
+    }
 
 
 }
